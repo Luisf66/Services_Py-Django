1c92f55985a9f3e5350b4a1704ff18105a342b4b
Luisf66 <Luisf311220@gmail.com>
2025-03-27 19:27:42 -0300

    refactor: use of abstract user in model user #3

--- users/models.py
+++ users/models.py
@@ -1,25 +1,33 @@
 from django.db import models
-#creation of the user model #3
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
 PROFILE_CHOICES = (
     ('client', 'Cliente'),
     ('professional', 'Profissional')
 )
 
-class User(models.Model):
-    name = models.CharField(max_length=150)
-    email = models.EmailField(max_length=150, unique=True)
+class CustomUser(AbstractUser):  # Herdando de AbstractUser
+    email = models.EmailField(unique=True)  #forçamos a unicidade
     profile = models.CharField(max_length=15, choices=PROFILE_CHOICES, default='client')
-    
+
+    def __str__(self):
+        return self.username  # Melhor usar username como identificador
+
 class PhoneNumber(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='user_phone_numbers')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='phone_numbers')
     number = models.CharField(max_length=15)
 
+    def __str__(self):
+        return f"{self.user.username} - {self.number}"
+
 class Address(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='user_addresses')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='addresses')
     street = models.CharField(max_length=150)
     neighborhood = models.CharField(max_length=100)
     house_number = models.CharField(max_length=5)
     city = models.CharField(max_length=150)
     state = models.CharField(max_length=150)
     zip_code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.street}, {self.city}, {self.house_number}"
