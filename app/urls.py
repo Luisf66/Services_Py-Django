3c0e0c65928b41ff1cbff7ecece1cc67ab7447a3
Luisf66 <Luisf311220@gmail.com>
2025-03-27 21:01:42 -0300

    feat: url register for user #5

--- app/urls.py
+++ app/urls.py
@@ -21,5 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # urls para serviços
     path('', include('services.urls')),
+    #urls para usuários
+    path('', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
