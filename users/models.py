d788cd8b7e7296612d193667c51ed92f8ab33fcd
Luisf66 <Luisf311220@gmail.com>
2025-03-27 21:03:42 -0300

    fix: correction in user model #3

--- users/models.py
+++ users/models.py
@@ -1,27 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
+# Definição das opções de perfil do usuário
 PROFILE_CHOICES = (
     ('client', 'Cliente'),
     ('professional', 'Profissional')
 )
 
-class CustomUser(AbstractUser):  # Herdando de AbstractUser
-    email = models.EmailField(unique=True)  #forçamos a unicidade
+class User(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(max_length=150, unique=True)  # Garantindo e-mails únicos
     profile = models.CharField(max_length=15, choices=PROFILE_CHOICES, default='client')
 
     def __str__(self):
-        return self.username  # Melhor usar username como identificador
+        return self.name
 
 class PhoneNumber(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='phone_numbers')
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='user_phone_numbers')
     number = models.CharField(max_length=15)
 
     def __str__(self):
-        return f"{self.user.username} - {self.number}"
+        return self.number
 
 class Address(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='addresses')
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='user_addresses')
     street = models.CharField(max_length=150)
     neighborhood = models.CharField(max_length=100)
     house_number = models.CharField(max_length=5)
@@ -30,4 +31,4 @@ class Address(models.Model):
     zip_code = models.CharField(max_length=20)
 
     def __str__(self):
-        return f"{self.user.username} - {self.street}, {self.city}, {self.house_number}"
+        return f'{self.street}, {self.street}, {self.city}, {self.house_number}'
